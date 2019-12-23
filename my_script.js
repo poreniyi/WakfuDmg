4d15838dd8648987b8d73766b84396e60ae6dbd6
Poreniyi <poreniyi@gmail.com>
2019-12-23 12:26:50 -0500

    Made index html js file and stylesheet

---
+++ my_script.js
@@ -0,0 +1,11 @@
+const spellDmg=document.querySelector('#spellDmg');
+const spellDmg=document.querySelector('#eleMast');
+const spellDmg=document.querySelector('#typeMast');
+const spellDmg=document.querySelector('#rangeMast');
+const spellDmg=document.querySelector('#critMast');
+const spellDmg=document.querySelector('#rearMast');
+const finalDmg=document.querySelector("#BeserkMast");
+finalDmg.textContent="Final Damage:";
+
+
+//calculations here
